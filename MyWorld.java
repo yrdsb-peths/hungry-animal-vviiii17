be066d9f725a917d53d639343c2b5ec289237e66
vviiii17 <150176425+vviiii17@users.noreply.github.com>
2023-11-22 15:35:16 -0500

    modified apple and elephant classes

--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * The World our hero lives in.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Yuvia
+ * @version November 2023
  */
 public class MyWorld extends World
 {
@@ -16,6 +16,23 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1); 
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
+        Apple apple = new Apple();
+        addObject(apple, 300, 0);
+    }
+    
+    /**
+     * Create a new apple at random location at top of screen
+     */
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = Greenfoot.getRandomNumber(_limit_);
+        addObject(apple, 300, 0);
     }
 }
