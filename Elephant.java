ed73f83174b9a1779ce586f690bb2d4a9de36d67
vviiii17 <150176425+vviiii17@users.noreply.github.com>
2023-11-23 23:50:25 -0500

    add eat() instance method

--- Elephant.java
+++ Elephant.java
@@ -25,6 +25,20 @@ public class Elephant extends Actor
         }
         
         // Remove apple if elephant eats it
-        removeTouching(Apple.class);
+        eat();
+    }
+    
+    /**
+     * Eat the apple and spawn new apple if an apple is eaten
+     */
+    
+    public void eat()
+    {
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+        }
     }
 }
