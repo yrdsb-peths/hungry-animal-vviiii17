b2714991733a96c4b6cec7333cb69f22c6c3b047
vviiii17 <150176425+vviiii17@users.noreply.github.com>
2023-11-27 13:12:34 -0500

    add Label and gameover

--- Elephant.java
+++ Elephant.java
@@ -39,6 +39,7 @@ public class Elephant extends Actor
             removeTouching(Apple.class);
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
+            world.increaseScore();
         }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    public int score = 0;
+    Label scoreLabel = new Label(0, 80);
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,12 +17,35 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1, false); 
         
+        // Create the elephant object
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 200);
+        addObject(elephant, 300, 300);
+        
+        // Create a label
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
         
         createApple();
+    }
+    
+    /**
+     * End the game and draw 'Game Over'
+     */
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
+    /**
+     * Increase score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
     }
     
     /**
@@ -31,7 +55,7 @@ public class MyWorld extends World
     {
         Apple apple = new Apple();
         int x = Greenfoot.getRandomNumber(600);
-        int y = Greenfoot.getRandomNumber(300);
-        addObject(apple, 300, 0);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
